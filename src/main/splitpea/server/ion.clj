0c6125c68ef7d35e463aad75b1e3f80940374831
Calvin Sauer <calvin.j.sauer@gmail.com>
2019-10-09 16:43:27 -0400

    Wire up existing resolvers and datomic connection to parser context

--- src/main/splitpea/server/ion.clj
+++ src/main/splitpea/server/ion.clj
@@ -1,11 +1,16 @@
 (ns splitpea.server.ion
   (:require [tightrope.server :as rope]
-            [datomic.ion.lambda.api-gateway :as apigw]))
+            [datomic.ion.lambda.api-gateway :as apigw]
+            [splitpea.server.db :as db]
+            [splitpea.resolvers :as shared-resolvers]
+            [splitpea.server.resolvers :as server-resolvers]))
 
 (def handler
-  (rope/tightrope-handler {:path "/api"
-                           :parser-opts {:env {}
-                                         :resolvers []}}))
+  (let [all-resolvers (concat shared-resolvers/all
+                              server-resolvers/all)]
+    (rope/tightrope-handler {:path "/api"
+                             :parser-opts {:env {:conn (db/get-conn)}
+                                           :resolvers all-resolvers}})))
 
 (def ionized-handler
   (apigw/ionize handler))
